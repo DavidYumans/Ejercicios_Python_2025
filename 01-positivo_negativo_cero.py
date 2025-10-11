de107664e2ada8de2f83a4fb51fc04d32cdee081
Your Name <you@example.com>
2025-10-11 10:36:26 -0600

    Ejercicio 1

---
+++ 01-positivo_negativo_cero.py
@@ -0,0 +1,9 @@
+print("Bienvenido")
+numero = float(input("Ingrese un Número: "))
+
+if numero > 0:
+    print("El número ingresado es positivo")
+elif numero < 0:
+    print("El numero ingresado es negativo")
+else:
+    print("El número ingresado es 0")
